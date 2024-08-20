470541ff1a318fd5954aa8518636a18ebf6bce26
boomzvpn789 <boomzvpn777@gmail.com>
2024-08-21 05:51:30 +0700

    Add files via upload

---
+++ application/views/panel/admin/message.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h3 class="page-header">การแจ้งเตือน
+            </h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                </div>
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>อันดับ</th>
+                                    <th>จาก</th>
+                                    <th>ข้อความ</th>
+                                    <th>วันที่</th>
+                                    <th>จัดการ</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php if(count($msg) !== 0): ?>
+                                        <?php foreach ($msg as $row): ?>
+                                            <tr>
+                                                <td><?= $row['userid'] ?></td>
+                                                <td><?= $row['username'] ?></td>
+                                                <td><?= $row['pesan']?></td>
+                                                <td><?= $row['created_at']?></td>
+                                                <td>
+                                                    <a href="<?= base_url('admin/konfirm_msg/').$row['id']?>" class="btn btn-info"> ยืนยัน</a>
+                                                </td>
+                                                <td>
+                                                    <a href="<?= base_url('admin/del_msg/').$row['id'] ?>" class="btn btn-danger"> ลบ</a>
+                                                </td>
+                                             </tr>
+                                       <?php endforeach; ?>
+                                    <?php else: ?>
+                                   
+                                        <tr>
+                                            <td class="text-muted text-center" colspan="6"> ไม่มีการแจ้งเตือน</td>
+                                        </tr>
+                                   <?php endif; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
