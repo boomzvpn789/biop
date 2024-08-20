a9b1c8c4104b458f15287bde424181768dc02d40
boomzvpn789 <boomzvpn777@gmail.com>
2024-08-21 06:04:46 +0700

    Delete wallet directory

--- wallet/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$host = 'localhost'; 
-$user = 'id4190951_0874542610';  // ตั้งค่า User ให้ตรงกับ MYSQL
-$pass = '0874542610';// ตั้งค่า passให้ตรงกับ MYSQL
-$db = 'id4190951_0874542610'; // ตั้งค่า db ให้ตรงกับ MYSQL
-$con = mysqli_connect($host,$user,$pass,$db); 
-mysqli_set_charset($con,'utf8');
-?>
